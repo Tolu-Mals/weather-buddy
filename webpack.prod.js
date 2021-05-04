551b6b0802ff65f7eb70cddbeec251c0aab34d03
tolu-mals <malomo.tolz@gmail.com>
2021-05-04 06:49:23 +0100

    Build desktop nav

--- webpack.prod.js
+++ webpack.prod.js
@@ -32,6 +32,25 @@ module.exports = {
           "sass-loader",
         ],
       },
+      // {
+      //   test: /\.(png|jpg|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //       options: {
+      //         limit: 8192,
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
     ],
   },
 };

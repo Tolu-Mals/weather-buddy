6e9ed93e475592518140776e418969eb1251e999
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2021-05-10 08:59:55 +0100

    Add some mobile styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.scss';
+
+const nav = document.getElementById('nav');
+const navBar = document.getElementById('nav-bar');
+const container = document.getElementById('container');
+const hamburger = document.getElementById('hamburger');
+
+
+//Testing stuff out
+// console.log(nav, navBar, container);
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    navBar.classList.toggle('open');
+    container.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});

fafab6b09d4b6ad0d4f8d887d785869a0bde9ff1
Tolulope Malomo <30352484+Tolu-Mals@users.noreply.github.com>
2021-05-17 08:11:31 +0100

    Complete version styling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import './style.scss';
 
+//NAVBAR
 const nav = document.getElementById('nav');
 const navBar = document.getElementById('nav-bar');
 const container = document.getElementById('container');
 const hamburger = document.getElementById('hamburger');
-
-
-//Testing stuff out
-// console.log(nav, navBar, container);
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('open');
     navBar.classList.toggle('open');
     container.classList.toggle('open');
     hamburger.classList.toggle('open');
+});
+
+
+//APIS
+const getWeatherBtn = document.getElementById('get-weather-btn');
+
+const getIP = () => {
+    fetch('https://api.bigdatacloud.net/data/client-ip')
+    .then((res)=> res.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+        console.log(err);
+    });
+};
+
+getWeatherBtn.addEventListener('click', () => {
+    getIP();
 });
